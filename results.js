81a0bc98567de1974dba00a0a6eae47b649de694
Irene <airine@Irenes-MacBook-Air.local>
2020-07-23 12:31:16 -0700

    [html] Add forms

---
+++ results.js
@@ -0,0 +1,23 @@
+const resultsDiv = document.querySelector('#results');
+console.log(resultsDiv);
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+    console.log(name);
+    console.log(value);
+
+    // let nameElement = document.createElement('p');
+    // let valueElement = document.createElement('p');
+
+    // nameElement.textContent = name;
+    // valueElement.textContent = value;
+
+    //console.log(nameElement)
+
+    // resultsDiv.appendChild(nameElement);
+    // resultsDiv.appendChild(valueElement);
+
+    let resultElement = document.createElement('p');
+    resultElement.textContent = name + ' ' + value;
+
+    resultsDiv.appendChild(resultElement);
+});
